da4a765d13b928757d75dc8965b2a02e3d78c297
Marcelo Ragnelli Frigério <marcelo_ragnelli@MacBook-Pro-de-Marcelo.local>
2019-11-19 03:23:17 -0300

    Convertida a aplicação para sintaxe Hooks

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import * as CartActions from '../../store/modules/cart/actions'
 import currencyFormatter from '../../utils/currencyFormatter'
@@ -23,80 +22,73 @@ import {
   CheckoutButtonText
 } from './styles'
 
-class Cart extends Component {
-  handleUpdateAmount(id, amount) {
-    const { updateAmountRequest } = this.props
-    updateAmountRequest(id, amount)
+export default function Cart() {
+  const dispatch = useDispatch()
+
+  function handleUpdateAmount(id, amount) {
+    dispatch(CartActions.updateAmountRequest(id, amount))
   }
 
-  handleRemoveProduct(id) {
-    const { removeProduct } = this.props
-    removeProduct(id)
+  function handleRemoveProduct(id) {
+    dispatch(CartActions.removeProduct(id))
   }
 
-  render() {
-    const { cart, totalPriceFormatted } = this.props
-    return (
-      <Container>
-        <CartContainer>
-          {cart.map(product => (
-            <CartItem key={product.id}>
-              <ItemSpecs>
-                <ItemImage source={{ uri: product.image }} />
-                <ItemDescription>
-                  <ItemTitle>{product.title}</ItemTitle>
-                  <ItemPrice>{product.priceFormatted}</ItemPrice>
-                </ItemDescription>
-                <Icon
-                  name="delete-forever"
-                  size={24}
-                  color="#7159c1"
-                  onPress={() => this.handleRemoveProduct(product.id)}
-                />
-              </ItemSpecs>
-              <ItemPriceBar>
-                <SelectPrice>
-                  <Icon
-                    name="minus-circle-outline"
-                    size={20}
-                    onPress={() => this.handleUpdateAmount(product.id, -1)}
-                  />
-                  <ItemAmount value={String(product.amount)} editable={false} />
-                  <Icon
-                    name="plus-circle-outline"
-                    size={20}
-                    onPress={() => this.handleUpdateAmount(product.id, 1)}
-                  />
-                </SelectPrice>
-                <ItemTotalPrice>{product.subtotal}</ItemTotalPrice>
-              </ItemPriceBar>
-            </CartItem>
-          ))}
-          <TotalLabel>TOTAL</TotalLabel>
-          <TotalPrice>{totalPriceFormatted}</TotalPrice>
-          <CheckoutButton>
-            <CheckoutButtonText>FINALIZAR PEDIDO</CheckoutButtonText>
-          </CheckoutButton>
-        </CartContainer>
-      </Container>
+  const { cart, totalPriceFormatted } = useSelector(state => ({
+    cart: state.cart.map(product => ({
+      ...product,
+      priceFormatted: currencyFormatter.format(product.price),
+      subtotal: currencyFormatter.format(product.price * product.amount)
+    })),
+    totalPriceFormatted: currencyFormatter.format(
+      state.cart.reduce((total, product) => {
+        total += product.price * product.amount
+        return total
+      }, 0)
     )
-  }
-}
+  }))
 
-const mapStateToProps = state => ({
-  cart: state.cart.map(product => ({
-    ...product,
-    priceFormatted: currencyFormatter.format(product.price),
-    subtotal: currencyFormatter.format(product.price * product.amount)
-  })),
-  totalPriceFormatted: currencyFormatter.format(
-    state.cart.reduce((total, product) => {
-      total += product.price * product.amount
-      return total
-    }, 0)
+  return (
+    <Container>
+      <CartContainer>
+        {cart.map(product => (
+          <CartItem key={product.id}>
+            <ItemSpecs>
+              <ItemImage source={{ uri: product.image }} />
+              <ItemDescription>
+                <ItemTitle>{product.title}</ItemTitle>
+                <ItemPrice>{product.priceFormatted}</ItemPrice>
+              </ItemDescription>
+              <Icon
+                name="delete-forever"
+                size={24}
+                color="#7159c1"
+                onPress={() => handleRemoveProduct(product.id)}
+              />
+            </ItemSpecs>
+            <ItemPriceBar>
+              <SelectPrice>
+                <Icon
+                  name="minus-circle-outline"
+                  size={20}
+                  onPress={() => handleUpdateAmount(product.id, -1)}
+                />
+                <ItemAmount value={String(product.amount)} editable={false} />
+                <Icon
+                  name="plus-circle-outline"
+                  size={20}
+                  onPress={() => handleUpdateAmount(product.id, 1)}
+                />
+              </SelectPrice>
+              <ItemTotalPrice>{product.subtotal}</ItemTotalPrice>
+            </ItemPriceBar>
+          </CartItem>
+        ))}
+        <TotalLabel>TOTAL</TotalLabel>
+        <TotalPrice>{totalPriceFormatted}</TotalPrice>
+        <CheckoutButton>
+          <CheckoutButtonText>FINALIZAR PEDIDO</CheckoutButtonText>
+        </CheckoutButton>
+      </CartContainer>
+    </Container>
   )
-})
-
-const mapDispatchToProps = dispatch => bindActionCreators(CartActions, dispatch)
-
-export default connect(mapStateToProps, mapDispatchToProps)(Cart)
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
+import React, { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 import currencyFormatter from '../../utils/currencyFormatter'
 import * as CartActions from '../../store/modules/cart/actions'
@@ -18,67 +17,59 @@ import {
 } from './styles'
 import api from '../../services/api'
 
-class Home extends Component {
-  state = {
-    products: []
+export default function Home() {
+  const [products, setProducts] = useState([])
+  const amount = useSelector(state =>
+    state.cart.reduce((amountObj, product) => {
+      amountObj[product.id] = product.amount
+      return amountObj
+    }, {})
+  )
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    async function loadProducts() {
+      const { data } = await api.get('/products')
+      const dataFormatted = data.map(product => {
+        product.priceFormatted = currencyFormatter.format(product.price)
+        return product
+      })
+      setProducts([...dataFormatted])
+    }
+
+    loadProducts()
+  }, [])
+
+  function handleAddProduct(id) {
+    dispatch(CartActions.addProductRequest(id))
   }
 
-  async componentDidMount() {
-    const { data } = await api.get('/products')
-    const dataFormatted = data.map(product => {
-      product.priceFormatted = currencyFormatter.format(product.price)
-      return product
-    })
-    this.setState({
-      products: [...dataFormatted]
-    })
-  }
-
-  handleAddProduct(id) {
-    const { addProductRequest } = this.props
-    addProductRequest(id)
-  }
-
-  render() {
-    const { amount } = this.props
-    const { products } = this.state
-    return (
-      <Container>
-        <ProductList
-          data={products}
-          keyExtractor={product => String(product.id)}
-          renderItem={({ item }) => (
-            <ProductCard>
-              <ProductImage source={{ uri: item.image }} />
-              <Title>{item.title}</Title>
-              <Price>{item.priceFormatted}</Price>
-              <AddButton onPress={() => this.handleAddProduct(item.id)}>
-                <BtnIcon>
-                  <Icon
-                    name="add-shopping-cart"
-                    size={24}
-                    color="#fff"
-                    style={{ marginRight: 5 }}
-                  />
-                  <IconText>{amount[item.id] || 0}</IconText>
-                </BtnIcon>
-                <BtnText>adicionar</BtnText>
-              </AddButton>
-            </ProductCard>
-          )}
-        />
-      </Container>
-    )
-  }
+  return (
+    <Container>
+      <ProductList
+        data={products}
+        keyExtractor={product => String(product.id)}
+        renderItem={({ item }) => (
+          <ProductCard>
+            <ProductImage source={{ uri: item.image }} />
+            <Title>{item.title}</Title>
+            <Price>{item.priceFormatted}</Price>
+            <AddButton onPress={() => handleAddProduct(item.id)}>
+              <BtnIcon>
+                <Icon
+                  name="add-shopping-cart"
+                  size={24}
+                  color="#fff"
+                  style={{ marginRight: 5 }}
+                />
+                <IconText>{amount[item.id] || 0}</IconText>
+              </BtnIcon>
+              <BtnText>adicionar</BtnText>
+            </AddButton>
+          </ProductCard>
+        )}
+      />
+    </Container>
+  )
 }
-
-const mapStateToProps = state => ({
-  amount: state.cart.reduce((amount, product) => {
-    amount[product.id] = product.amount
-    return amount
-  }, {})
-})
-
-const mapDispatchToProps = dispatch => bindActionCreators(CartActions, dispatch)
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home)
